0f2f38ea61beb88d29f7341889241241b48ea468
D1TH <d1th@telia.com>
2013-02-25 20:38:18 +0100

    inistal commit

---
+++ client/doorkeeper.js
@@ -0,0 +1,35 @@
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+
+
+ Template.asd.events({
+ 	'keyup #url' : function(evt) {
+ 		if(evt.keyCode == 13){
+	 		var query = $('#url').val();
+ 			Session.set("lastSearch", query);
+ 			Searches.insert({query: query});
+		 	var googleMatcher=/^google /i;		//google search commnad
+		 	var hexMatcher=/^#[0123456789abcdef]+$/i;
+		 	if(googleMatcher.test(query)){
+		 		window.location = 'http://www.google.com/search?q=' + query.substring(7);
+			} else if(hexMatcher.test(query)) {
+	 			$('body')[0].style['background-color']=query;
+	 			$('#url').val("");
+			} else {
+			 	window.location = query.toLowerCase().replace(/ /g, '-'); 		//Replace "space" to "-"	
+			}
+		}
+ 	}
+ });
+
+ //Template.lastSearch.lastSearch = function() {
+ //	return Session.get("lastSearch");
+ //};
+
+ //Template.lastSearch.lastSearches = function() {
+ //	return Searches.find();
+ //};
---
+++ common.js
@@ -0,0 +1,1 @@
+Searches = new Meteor.Collection("searches");
